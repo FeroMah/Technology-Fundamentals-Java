ce941ec1c78ab4d052e18a7bb5e4d6dcbe31e91c
Ferhat Mahmud <ferhatmahmud@gmail.com>
2020-06-28 00:58:20 +0300

    Initial commit.

--- 05.Methods - Exercise/src/TestAsd.java
+++ 05.Methods - Exercise/src/TestAsd.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VladoTest {
+public class TestAsd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = Integer.parseInt(sc.nextLine());
